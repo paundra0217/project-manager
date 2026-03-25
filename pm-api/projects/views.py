7178606615eb464cb6861c97e7833504ebd5a77b
paundra0217 <45008116+paundra0217@users.noreply.github.com>
2026-03-25 16:51:33 +0700

    feat: GET returns is_archived, and PATCH will prevent modification when project is archived (#9)

--- pm-api/projects/views.py
+++ pm-api/projects/views.py
@@ -75,14 +75,21 @@ class ProjectBotView():
     
     @api_view(['GET', 'PATCH', 'DELETE'])
     def project_detail(request, id):
-        if request.method == 'GET':
-            return ProjectBotView.get_project(request, id)
+        match request.method:
+            case 'GET':
+                return ProjectBotView.get_project(request, id)
 
-        elif request.method == 'PATCH':
-            return ProjectBotView.edit_project(request, id)
+            case 'PATCH':
+                return ProjectBotView.edit_project(request, id)
 
-        elif request.method == 'DELETE':
-            return ProjectBotView.delete_project(request, id)
+            case 'DELETE':
+                return ProjectBotView.delete_project(request, id)
+            
+            case _:
+                return Response(
+                    {"message": "Method not allowed"},
+                    status=status.HTTP_405_METHOD_NOT_ALLOWED,
+                )
         
     def get_project(request, id):
         project = ProjectBoard.objects.filter(id=id).first()
@@ -100,22 +107,57 @@ class ProjectBotView():
     def edit_project(request, id):
         name = request.data.get("name")
         description = request.data.get("description")
-        user = request.data.get("user")
         channel = request.data.get("channel")
         message = request.data.get("message")
+        is_archived = request.data.get("is_archived")
 
-        project = ProjectBoard.objects.get(id=id)
-        project.name = name if name else project.name
-        project.description = description if description else project.description
-        project.updated_by = user if user else project.user
-        project.channel_id = channel if channel else project.channel_id
-        project.message_id = message if message else project.message_id
-        project.save()
-
-        return Response(
-                {"project": ProjectBoardSerializer(project).data},
-                status=status.HTTP_200_OK,
+        user = request.data.get("user")
+        if not user:
+            return Response(
+                {"message": "User ID is missing"},
+                status=status.HTTP_400_BAD_REQUEST,
             )
+
+        try:
+            project = ProjectBoard.objects.get(id=id)
+            if project.is_archived is True:
+                if is_archived is not None and is_archived is False:
+                    project.updated_by = user
+                    project.is_archived = False
+                    project.save()
+                    return Response(
+                        {"project": ProjectBoardSerializer(project).data},
+                        status=status.HTTP_200_OK,
+                    )
+                else:
+                    return Response(
+                        {"message": "Project is archived and cannot be modified."},
+                        status=status.HTTP_409_CONFLICT,
+                    )
+            else:
+                project.updated_by = user
+                project.name = name if name else project.name
+                project.description = description if description else project.description
+                project.channel_id = channel if channel else project.channel_id
+                project.message_id = message if message else project.message_id
+                project.is_archived = is_archived if is_archived else project.is_archived
+                project.save()
+
+                return Response(
+                        {"project": ProjectBoardSerializer(project).data},
+                        status=status.HTTP_200_OK,
+                    )
+        except ProjectBoard.DoesNotExist:
+            return Response(
+                    {"message": "Project does not exist"},
+                    status=status.HTTP_200_OK,
+                )
+        except Exception as e:
+            print(e)
+            return Response(
+                    {"message": "Internal Server Error"},
+                    status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                )
     
     def delete_project(request, id):
         project = ProjectBoard.objects.get(id=id)
